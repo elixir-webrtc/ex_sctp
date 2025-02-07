59997949abaa4fb7a632c9a612189ef33f987781
Michał Śledź <michalsledz34@gmail.com>
2025-02-07 17:54:38 +0100

    Update deps. Bump version to 0.1.2

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Add `ex_sctp` to dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:ex_sctp, "~> 0.1.0"}
+    {:ex_sctp, "~> 0.1.2"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExSCTP.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/elixir-webrtc/ex_sctp"
 
   def project do
@@ -58,7 +58,7 @@ defmodule ExSCTP.MixProject do
       {:ex_doc, "~> 0.31", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:rustler, "~> 0.34.0", runtime: false}
+      {:rustler, "~> 0.36.0", runtime: false}
     ]
   end
 
